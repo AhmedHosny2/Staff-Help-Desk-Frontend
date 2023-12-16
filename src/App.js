58851d7df10332a093622c5b7037a93a24c0ccac
bahylol <94308278+bahylol@users.noreply.github.com>
2023-12-16 16:56:08 +0200

    added routes for the new pages

--- src/App.js
+++ src/App.js
@@ -19,6 +19,9 @@ import TestPage from './pages/test/test.jsx';
 import Login from './pages/login/login.jsx';
 import Signup from './pages/signup/signup.jsx';
 import Profile from './pages/profile/profile.jsx';
+import Logs from './pages/Logs/Logs.jsx';
+import ManageUsers from './pages/ManageUsers/ManageUsers.jsx';
+import AddUser from './pages/AddUser/AddUser.jsx';
 
 function App() {
 	const location = useLocation();
@@ -34,6 +37,9 @@ function App() {
 					<Route path="/login" element={<Login setLoggedin={setLoggedin} />} />
 					<Route path="/signup" element={<Signup />} />
 					<Route path="/profile" element={<Profile />} />
+					<Route path="/logs" element={<Logs />} />
+					<Route path="/manageUsers" element={<ManageUsers />} />
+					<Route path="/AddUser" element={<AddUser />} />
 				</Routes>
 			</AnimatePresence>
 		</>

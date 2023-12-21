e3d13b2eb2c9e28fc63a412512602331d183c9c9
MooTamer <mohamedtamer200200@gmail.com>
2023-12-21 23:31:46 +0200

    modal for change role

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
-		extend: {},
+		
+		extend: {
+			backdropBlur: {
+				'3': '3px'
+			  }
+		},
 	},
 	plugins: [require('daisyui')],
 

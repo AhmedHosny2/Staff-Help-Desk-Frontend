eb3d459ec621ff9fd367a3cc56bb56dfe296d264
Ahmed Yehia <Ahmed.Hosny4434@gmail.com>
2023-12-22 07:42:01 +0200

    save and update ticket styling

--- src/pages/TicketEntity/components/WYSIWYG.jsx
+++ src/pages/TicketEntity/components/WYSIWYG.jsx
@@ -1,19 +1,48 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import JoditEditor from "jodit-react";
 import updateButton from "../../../assets/updateButton.svg";
 import saveButton from "../../../assets/saveButton.svg";
+import { customFetch } from "../../../utils/Fetch";
+import { useNavigate } from "react-router-dom";
 
 export default function WYSIWYG() {
+  const navigate = useNavigate();
+  const [isPending, setIsPending] = useState(false);
+  const [data, setData] = useState(null);
+
+  // put this in use effect
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       if (!localStorage.getItem("role")) navigate("/");
+  //       setUserType(localStorage.getItem("role"));
+  //
+  //     } catch (error) {
+  //       console.error("Error fetching data:", error);
+  //     }
+  //   };
+  //   fetchData();
+  // }, []);
   const editor = useRef(null);
   const [content, setContent] = useState("");
-
-  const handleSolveButton = () => {
-    console.log(editor.current);
+    const [status, setStatus] = useState(null);
+  const handleButtonClick = async  (e) => {
+    console.log(e.target.value);
+    const body = {
+      ticketId: window.location.pathname.split("/")[2],
+      solution: editor.current.value,
+      status: e.target.value,
+    };
+    const { newStatus } = await customFetch(
+      process.env.REACT_APP_TICKETS_URL + "/solveTicket",
+      "PUT",
+      body
+    );
+    setStatus(newStatus);
+    if(newStatus === 200)navigate("/ticket");
+    else alert("Error");
   };
 
-  const handleUpdateButton = () => {
-    console.log(editor.current);
-  };
 
   return (
     <div className="flex items-center justify-center h-screen">
@@ -24,14 +53,16 @@ export default function WYSIWYG() {
         <div className="flex justify-between mt-8">
           <button
             className="flex items-center btn btn-success"
-            onClick={handleSolveButton}
+            value="closed"
+            onClick={handleButtonClick}
           >
             <img src={saveButton} alt="Save & Close Ticket" className="mr-2" />
             Save & Close Ticket
           </button>
           <button
             className="flex items-center btn btn-info"
-            onClick={handleUpdateButton}
+            value="updated"
+            onClick={handleButtonClick}
           >
             <img src={updateButton} alt="Update" className="mr-2" />
             Update

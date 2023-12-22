77da96d0f5fc890283cd6c772fba7e2bd955360a
Ahmed Yehia <Ahmed.Hosny4434@gmail.com>
2023-12-22 11:34:54 +0200

    search bar by issues type

--- src/pages/Tickets/components/userTicket.jsx
+++ src/pages/Tickets/components/userTicket.jsx
@@ -1,40 +1,52 @@
-import { Link, useNavigate } from "react-router-dom";
-import QRCode from "react-qr-code";
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import QRCode from 'react-qr-code';
 
 export default function UserTicket({ data }) {
   const navigate = useNavigate();
+  const [searchQuery, setSearchQuery] = useState('');
 
-  const handleButtonClick = (e) => {
-    navigate(`/ticketEntity/${e.currentTarget.dataset.ticketId}`);
+  const handleButtonClick = (ticketId) => {
+    navigate(`/ticketEntity/${ticketId}`);
   };
+
+  const filteredData = data.filter((ticketData) =>
+    ticketData.issue_type.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
     <>
-      {data &&
-        data.map((ticketData) => (
-          <div
-            key={ticketData._id}
-            className="card w-96 bg-base-100 shadow-xl"
-            onClick={handleButtonClick}
-            data-ticket-id={ticketData._id}
-          >
-            {" "}
-            <figure>
-              <QRCode value={window.location.href} />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">
-                {ticketData.title}
-                <div className="badge badge-secondary">{ticketData.status}</div>
-              </h2>
-              <p>{ticketData.description}</p>
-              <div className="card-actions justify-end">
-                <div className="badge badge-outline">
-                  {ticketData.issue_type}
-                </div>
-              </div>
+      <div className="mb-4">
+        <input
+          type="text"
+          placeholder="Search by issue type"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          className="input input-bordered input-primary w-full max-w-xs"
+        />
+      </div>
+
+      {filteredData.map((ticketData) => (
+        <div
+          key={ticketData._id}
+          className="card w-96 bg-base-100 shadow-xl mb-4"
+          onClick={() => handleButtonClick(ticketData._id)}
+        >
+          <figure>
+            <QRCode value={window.location.href} />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">
+              {ticketData.title}
+              <div className="badge badge-secondary">{ticketData.status}</div>
+            </h2>
+            <p>{ticketData.description}</p>
+            <div className="card-actions justify-end">
+              <div className="badge badge-outline">{ticketData.issue_type}</div>
             </div>
           </div>
-        ))}
+        </div>
+      ))}
     </>
   );
 }

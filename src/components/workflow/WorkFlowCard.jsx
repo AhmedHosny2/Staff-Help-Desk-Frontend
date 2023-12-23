bce0d92570430a4c9288972a95d005f1da4136d1
bahylol <94308278+bahylol@users.noreply.github.com>
2023-12-23 21:20:06 +0200

    fixed overflow

--- src/components/workflow/WorkFlowCard.jsx
+++ src/components/workflow/WorkFlowCard.jsx
@@ -1,9 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { getToastStyle, removeToast } from '../../utils/toastStyle';
 import toast, { Toaster } from 'react-hot-toast';
 
 const WorkflowCard = ({ cardId, editedText, onTextChange, onRemoveClick }) => {
     const [isEditing, setIsEditing] = useState(false);
+    const [inputHeight, setInputHeight] = useState('auto');
+    const inputRef = useRef(null);
+
+    useEffect(() => {
+        if (isEditing) {
+            setInputHeight(`${inputRef.current.scrollHeight}px`);
+        }
+    }, [isEditing, editedText]);
 
     const handleEditClick = () => {
         setIsEditing(true);
@@ -28,14 +36,15 @@ const WorkflowCard = ({ cardId, editedText, onTextChange, onRemoveClick }) => {
             <div className="card w-96 bg-base-100 shadow-xl">
                 <div className="card-body">
                     {isEditing ? (
-                        <input
-                            type="text"
+                        <textarea
+                            ref={inputRef}
                             value={editedText}
                             onChange={(e) => onTextChange(cardId, e.target.value)}
                             className="border-2 border-gray-300 h-8 px-3 pr-6 rounded-lg text-sm focus:outline-none"
+                            style={{ height: inputHeight, width: '100%', boxSizing: 'border-box', whiteSpace: 'pre-wrap' }}
                         />
                     ) : (
-                        <p>{editedText}</p>
+                        <p style={{ whiteSpace: 'pre-wrap' }}>{editedText}</p>
                     )}
 
                     <div className="card-actions justify-end mt-4">

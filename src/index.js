6f45603fd590670ae6aa43bb89a392dc692e2067
bahylol <94308278+bahylol@users.noreply.github.com>
2023-12-26 20:18:22 +0200

    PWA configs

--- src/index.js
+++ src/index.js
@@ -5,6 +5,18 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+if ('serviceWorker' in navigator) {
+	window.addEventListener('load', () => {
+	  navigator.serviceWorker.register('/service-worker.js')
+		.then(registration => {
+		  console.log('ServiceWorker registration successful:', registration);
+		})
+		.catch(error => {
+		  console.log('ServiceWorker registration failed:', error);
+		});
+	});
+  }
+  
 root.render(
 	<React.StrictMode>
 		<BrowserRouter>
